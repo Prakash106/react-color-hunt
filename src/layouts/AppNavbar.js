ec16e8ed603078b2d9bac7c7d08958b273dd3632
Prakash <prakash.hartron@gmail.com>
2019-05-22 20:21:45 +0530

    Typo mistake fixed

--- src/layouts/AppNavbar.js
+++ src/layouts/AppNavbar.js
@@ -6,7 +6,7 @@ import Logo from '../assets/color-hunt-palettes-logo.png'
 function AppNavbar () {
     return (
         <Navbar className="padding-both soft_shadow" collapseOnSelect expand='lg' bg='white' variant='light'>
-        <Navbar.Brand href='#home'><img src={Logo} className="logo" /> Color Hunt - React</Navbar.Brand>
+        <Navbar.Brand href='#home'><img src={Logo} className="logo" /> ColorHunt - React</Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav className='mr-auto theme-nav'>
